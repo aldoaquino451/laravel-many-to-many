5075ad40fadf7b7f6ac0d51545a523cb1391141c
aldoaquino451 <aldoaquino2909@gmail.com>
2023-12-04 16:22:54 +0100

    [init]

---
+++ app/Models/Type.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use App\Models\Project;
+
+class Type extends Model
+{
+    use HasFactory;
+
+    public function project() {
+        return $this->belongsTo(Project::class);
+    }
+    public static function generateSlug($name){
+        $slug = Str::slug($name, '-');
+        $original_slug = $slug;
+        $exist = Type::where('slug', $slug)->first();
+        $c = 1;
+        while($exist){
+            $slug = $original_slug. '-'. $c;
+            $exist = Type::where('slug', $slug)->first();
+            $c++;
+        }
+        return $slug;
+    }
+}
